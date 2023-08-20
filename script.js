78550f088b27e6cfb1cef0ca1ff9644216bbf173
JordanDorsey <jordandorsey04@gmail.com>
2023-08-20 11:39:54 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content was added by the developers of javaScript. Disclaimer; This webiste is for testing purposes only! Any use must be conducted under the authorization of oathe 23.19.M.Inc. Any violations of this order will cause the prosecuter to be detained and potential put under arrest depending on the offence. Do not touch."
+document.querySelector("body").appendChild(h2);
